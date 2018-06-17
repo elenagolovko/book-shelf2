378dc82e2e896dfcbdfaa486084f68a7d8fc97c5
elenagolovko <airdragon220@gmail.com>
2018-06-17 10:20:23 +0300

    logger & fixing probs

--- src/app/BooksList/books-list.component.ts
+++ src/app/BooksList/books-list.component.ts
@@ -7,7 +7,12 @@ import { Book } from '../data/book';
   template: `
     <div>
         <h3>Добавленные книги</h3>
+        <button class="sort-btn" type="button" (click)="nameSort()">Sort by name</button>
+        <button class="sort-btn" type="button" (click)="dateSort()">Sort by release date</button>
         <ul *ngFor="let book of books; let index = index" class="book-list">
+            <li class="img-upload book-element">
+                <img src="{{book.image}}" width="115" height="150" alt="Обложка книги">
+            </li>
             <li class="book-element">
             {{book.title}} <br> 
             {{book.author}} <br> 
@@ -33,5 +38,13 @@ export class BooksListComponent {
 
     removeBook (ind: number) {
         this.bookService.removeData(ind);
+    }
+
+    nameSort() {
+        this.bookService.sortNames();
+    }
+
+    dateSort(){
+        this.bookService.sortDates();
     }
 }
--- src/app/data/book.service.ts
+++ src/app/data/book.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from "@angular/core";
 // import { HttpClient } from "@angular/common/http";
+import { Logger } from './logger.service';
 import { Book } from "./book";
 
 @Injectable()
+
 export class BookService {
+  private logger: Logger;
   private books: Book[] = [];
   private data: Book[] = JSON.parse(localStorage.getItem('books'));
+  save(){
+    localStorage.setItem('books', JSON.stringify(this.data));
+  }
 
   fetchData() {
     return fetch('../books.json')
@@ -13,17 +19,39 @@ export class BookService {
       .then(data => {
         this.books = data.bookList;
         localStorage.setItem('books', JSON.stringify(this.books));
-      });
+      })
+      .catch(err => {
+        this.logger.error(err);
+        throw err;
+      })
   }
   getData(): Book[] {
     return this.data;
   }
   addNew(book: any) {
     this.data.push(book);
-    localStorage.setItem('books', JSON.stringify(this.data));
+    this.save();
   }
   removeData(ind: number) {
     this.data.splice(ind, 1);
-    localStorage.setItem('books', JSON.stringify(this.data));
+    this.save();
+  }
+  sortNames() {
+    this.data.sort(function (a, b) {
+      if (a.title.toLowerCase() > b.title.toLowerCase()) {
+      return 1;
+      }
+      if (a.title.toLowerCase() < b.title.toLowerCase()) {
+      return -1;
+      }
+      return 0;
+    });
+    this.save();
+  }
+  sortDates(){
+    this.data.sort(function (a, b) {
+      return a.releaseDate - b.releaseDate;
+    });
+    this.save();
   }
 }
